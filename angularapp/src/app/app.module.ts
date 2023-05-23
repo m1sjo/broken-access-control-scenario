c80231f3a54d7b212dfd55471e6afaddabef5bb0
Patrick Pusch <120772@fhwn.ac.at>
2023-05-23 21:48:00 +0200

    added a lot of coppied code :))

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,15 +3,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Dashboard } from './components/pages/dashboard/dashboard';
+import { LoginPage } from './components/pages/loginPage/loginPage';
 
 @NgModule({
   declarations: [
     AppComponent
+    , LoginPage
+    , Dashboard
   ],
   imports: [
     BrowserModule, HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+    , LoginPage
+    , Dashboard]
 })
 export class AppModule { }
--- angularapp/src/app/components/pages/dashboard/dashboard.ts
+++ angularapp/src/app/components/pages/dashboard/dashboard.ts
@@ -1,1 +1,25 @@
-console.log("Hello World!")
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './dashboard.html',
+  styleUrls: ['./dashboard.css']
+})
+export class Dashboard {
+  public homeInfo?: SmartHomeInfo[];
+
+  constructor(http: HttpClient) {
+    http.get<SmartHomeInfo[]>('/smarthomeinfo').subscribe(result => {
+      this.homeInfo = result;
+    }, error => console.error(error));
+  }
+
+  title = 'ThisIsSMARThome';
+}
+
+interface SmartHomeInfo {
+  temperature: number;
+  humidity: number;
+  lightState: boolean;
+}
