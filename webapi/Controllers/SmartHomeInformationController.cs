4986e6072ff8352cfa960b0f98171b4aec17a0f9
HowAboutACupOfTea <karnerdaniel1@gmail.com>
2023-05-29 00:20:53 +0200

    Add controller for getting information about a smart home. Add model for information about a smart home.

---
+++ webapi/Controllers/SmartHomeInformationController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi.Model;
+
+namespace webapi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SmartHomeInformationController : ControllerBase
+{
+    private static readonly Random _random = new();
+
+    // Alternatively get from database.
+    private readonly SmartHomeInformation _smartHomeInformation = new()
+    {
+        Temperatur = _random.Next(-10, 31),
+        Humidity = _random.Next(0, 101),
+        IsLightOn = _random.Next(0,2) == 1,
+    };
+
+    [HttpGet(Name = "GetSmartHomeInformation")]
+    public SmartHomeInformation Get()
+    {
+        return _smartHomeInformation;
+    }
+
+    [HttpGet("GetTemperature")]
+    public int GetTemperature()
+    {
+        return _smartHomeInformation.Temperatur;
+    }
+
+    [HttpGet("GetHumidity")]
+    public int GetHumidity()
+    {
+        return _smartHomeInformation.Humidity;
+    }
+
+    [HttpGet("GetLightState")]
+    public bool GetLightState()
+    {
+        return _smartHomeInformation.IsLightOn;
+    }
+}
